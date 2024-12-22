c1048ba26ed2802b10da96f3825ec724225ef7be
biancalautaru <bialautaru@gmail.com>
2024-12-22 20:41:20 +0200

    Update HTMLPrettyPrinter.sh

--- HTMLPrettyPrinter.sh
+++ HTMLPrettyPrinter.sh
@@ -10,31 +10,33 @@ fi
 singletons=("<area>" "<base>" "<br>" "<col>" "<command>" "<embed>" "<hr>" "<img>" "<input>" "<keygen>" "<link>" "<meta>" "<param>" "<source>" "<track>" "<wbr>")
 inline=("<abbr>" "<acronym>" "<b>" "<bdo>" "<br>" "<button>" "<cite>" "<code>" "<dfn>" "<em>" "<i>" "<kbd>" "<q>" "<samp>" "<small>" "<span>" "<strong>" "<sub>" "<sup>" "<time>" "<var>")
 
+text=$(<"$input_file")
+
 output_file="pretty_$input_file"
 
 > $output_file
 
 level=-1
 
-cat $input_file | while IFS="" read -r line || [[ -n "$line" ]]; do
+for (( i=0; i<${#text}; i++ )); do
     poz=0
-    for (( i=0; i<${#line}; i++ )); do
-        if [[ "${line:$i:3}" == "<!d" ]] || [[ "${line:$i:3}" == "<!D" ]]; then # cazul doctype
-            echo -n "${line:$i:15}" >> $output_file
+    for (( i=0; i<${#text}; i++ )); do
+        if [[ "${text:$i:3}" == "<!d" ]] || [[ "${text:$i:3}" == "<!D" ]]; then # cazul doctype
+            echo -n "${text:$i:15}" >> $output_file
             poz=$i+16
             break
         fi
     done
     
-    for (( i=$poz; i<${#line}; i++ )); do
-        if [[ "${line:$i:1}" == "<" ]]; then
-            if [[ "${line:$i+1:1}" != "/" ]]; then # tag de deschidere
+    for (( i=$poz; i<${#text}; i++ )); do
+        if [[ "${text:$i:1}" == "<" ]]; then
+            if [[ "${text:$i+1:1}" != "/" ]]; then # tag de deschidere
                 tag=""
                 ok=0
                 sg=0
                 inl=0
-                for (( j=$i; j<${#line}; j++ )); do
-                    if [[ "${line:$j:1}" == " " ]] || [[ "${line:$j:1}" == ">" ]] && [[ $ok -eq 0 ]]; then
+                for (( j=$i; j<${#text}; j++ )); do
+                    if [[ "${text:$j:1}" == " " ]] || [[ "${text:$j:1}" == ">" ]] && [[ $ok -eq 0 ]]; then
                         ok=1
                         tag_low=$(echo "$tag" | tr "[A-Z]" "[a-z]")
 
@@ -53,8 +55,8 @@ cat $input_file | while IFS="" read -r line || [[ -n "$line" ]]; do
                         done
                     fi
 
-                    tag+=${line:$j:1}
-                    if [[ "${line:$j:1}" == ">" ]]; then
+                    tag+=${text:$j:1}
+                    if [[ "${text:$j:1}" == ">" ]]; then
                         level=$level+1 # pune tag pe stiva
                         if [[ $inl -eq 1 ]]; then # daca e tag inline, nu pun taburi si newline
                             echo -n "$tag" >> $output_file
@@ -75,12 +77,61 @@ cat $input_file | while IFS="" read -r line || [[ -n "$line" ]]; do
                     fi
                 done
 
+                if [[ $sg -eq 0 ]]; then
+                    for (( j=$i+1; j<${#text}; j++ )); do
+                        if [[ "${text:$j:1}" == "<" ]]; then # ajunge la urmatorul tag
+                            break
+                        fi
+
+                        if [[ "${text:$j:1}" != " " ]] && [[ "${text:$j:1}" != $'\n' ]] && [[ "${line:$j:1}" != $'\t' ]]; then # aici incepe textul
+                            if [[ $inl -eq 0 ]]; then
+                                echo -n $'\n' >> $output_file
+                                for (( k=1; k<=$level+1; k++ )); do
+                                    echo -n $'\t' >> $output_file
+                                done
+                            fi
+
+                            content=""
+                            while (( j<${#text} )); do # afiseaza caracterele pana la urmatorul tag
+                                if [[ "${text:$j:1}" == "<" ]]; then
+                                    if [[ "${text:$j+1:1}" != "/" ]]; then
+                                        echo -n "$content" >> $output_file
+                                    fi
+                                    break
+                                fi
+                                content+="${text:j:1}"
+                                if [[ "${text:$j:1}" != " " ]] && [[ "${text:$j:1}" != $'\n' ]] && [[ "${line:$j:1}" != $'\t' ]]; then
+                                    echo -n "$content" >> $output_file
+                                    content=""
+                                fi
+                                j=$j+1
+                            done
+                            break
+                        fi
+                    done
+                    i=$j-1
+                
+                else
+                    content=""
+                    for (( j=$i+1; j<${#text}; j++ )); do
+                        if [[ "${text:$j:1}" == "<" ]]; then # ajunge la urmatorul tag
+                            break
+                        fi
+                        content+="${text:j:1}"
+                        if [[ "${text:$j:1}" != " " ]] && [[ "${text:$j:1}" != $'\n' ]] && [[ "${line:$j:1}" != $'\t' ]]; then
+                            echo -n "$content" >> $output_file
+                            content=""
+                        fi
+                    done
+                    i=$j-1
+                fi
+
             else # tag de inchidere
                 tag=""
                 ok=0
                 inl=0
-                for (( j=$i; j<${#line}; j++ )); do
-                    if [[ "${line:$j:1}" == " " ]] || [[ "${line:$j:1}" == ">" ]] && [[ $ok -eq 0 ]]; then
+                for (( j=$i; j<${#text}; j++ )); do
+                    if [[ "${text:$j:1}" == " " ]] || [[ "${text:$j:1}" == ">" ]] && [[ $ok -eq 0 ]]; then
                         ok=1
                         tag_low=$(echo "<${tag:2:${#tag}}" | tr "[A-Z]" "[a-z]")
 
@@ -92,8 +143,8 @@ cat $input_file | while IFS="" read -r line || [[ -n "$line" ]]; do
                         done
                     fi
 
-                    tag+=${line:$j:1}
-                    if [[ "${line:$j:1}" == ">" ]]; then
+                    tag+=${text:$j:1}
+                    if [[ "${text:$j:1}" == ">" ]]; then
                         if [[ $inl -eq 1 ]]; then # daca e tag inline, nu pun taburi si newline
                             echo -n "$tag" >> $output_file
                         else
@@ -110,31 +161,20 @@ cat $input_file | while IFS="" read -r line || [[ -n "$line" ]]; do
                         break
                     fi
                 done
-            fi
 
-        else
-            for (( j=$i; j<${#line}; j++ )); do
-                if [[ "${line:$j:1}" == "<" ]]; then
-                    break
-                fi
-                if [[ "${line:$j:1}" != " " ]] && [[ "${line:$j:1}" != $'\n' ]] && [[ "${line:$j:1}" != $'\t' ]]; then # aici incepe textul
-                    if [[ $inl -eq 0 ]]; then # daca nu suntem intr-un tag inline
-                        echo -n $'\n' >> $output_file # pune newline dupa tagul de deschidere
-                        for (( k=1; k<=$level+1; k++ )); do
-                            echo -n $'\t' >> $output_file # pune taburi la inceputul textului
-                        done
+                content=""
+                for (( j=$i+1; j<${#text}; j++ )); do
+                    if [[ "${text:$j:1}" == "<" ]]; then # ajunge la urmatorul tag
+                        break
                     fi
-                    while (( j<${#line} )); do # parcurge caracterele pana la urmatorul tag
-                        if [[ "${line:$j:1}" == "<" ]]; then
-                            break
-                        fi
-                        echo -n "${line:$j:1}" >> $output_file
-                        j=$j+1
-                    done
-                    i=$j-1
-                    break
-                fi
-            done
+                    content+="${text:j:1}"
+                    if [[ "${text:$j:1}" != " " ]] && [[ "${text:$j:1}" != $'\n' ]] && [[ "${line:$j:1}" != $'\t' ]]; then
+                        echo -n "$content" >> $output_file
+                        content=""
+                    fi
+                done
+                i=$j-1
+            fi
         fi
     done
 done
